ff70433423390cf1a49628186a5abd56ec02706b
sp-va <vadis99@ya.ru>
2024-02-21 23:07:11 +0500

    /delete_picture эндпоинт (не завершен)

--- src/routers/documents.py
+++ src/routers/documents.py
@@ -8,6 +8,7 @@ from fastapi import APIRouter, UploadFile, File, status, HTTPException
 from fastapi.responses import FileResponse
 
 from transactions.insert_data import upload_picture
+from transactions.delete_data import delete_picture
 from db.base import AsyncSessionLocal
 
 router = APIRouter(
@@ -16,7 +17,7 @@ router = APIRouter(
 )
    
 @router.post("/upload_file", status_code=status.HTTP_201_CREATED)
-async def documents_root(picture: UploadFile = File(...)):
+async def upload_file(picture: UploadFile = File(...)):
     picture_uuid = uuid4()
     file_path = f"{Path(__file__).parent.parent}/media/{picture_uuid}.png"
     content = await picture.read()
@@ -33,4 +34,9 @@ async def documents_root(picture: UploadFile = File(...)):
                 file_path=file_path,
             )
         except:
-            raise HTTPException(status_code=400, detail="Возникла ошибка при сохранении изображения или при добавлении записи о нем в базу данных.") 
+            raise HTTPException(status_code=400, detail="Возникла ошибка при сохранении изображения или при добавлении записи о нем в базу данных.")
+
+@router.delete("/delete_file", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_file(id: str):
+    psth = delete_picture(async_session=AsyncSessionLocal, id=id)
+    print(await psth)
